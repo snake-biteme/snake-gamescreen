ebc504afb3b6d5ae783a7a6233dd5dcef27be9e0
Tina <tina.zhou.hui@gmail.com>
2022-04-20 17:14:45 +0100

    fix: out of bound random values

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,7 +1,7 @@
 import {COLUMNS, ROWS} from "../CONST";
 
 function randomIntFromInterval(min: number, max: number) { // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min) + min)
 }
 
 export function getRandomRow() {
