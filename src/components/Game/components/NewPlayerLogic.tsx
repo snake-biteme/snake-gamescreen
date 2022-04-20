f4171a082cfe187b1d515e866affa3f5e4ee99e8
Tina <tina.zhou.hui@gmail.com>
2022-04-20 17:46:40 +0100

    feat: get all players from prev state

--- src/components/Game/components/NewPlayerLogic.tsx
+++ src/components/Game/components/NewPlayerLogic.tsx
@@ -39,26 +39,7 @@ function NewPlayerLogic({setPlayers, setPositions}: IProps) {
 
 
     useEffect(() => {
-        const realtimeResults = (data: IRealTimeData) => {
-            const position = data.data.onPositionUpdated;
-
-            console.log('realtime data: ', position);
-            setPlayers((prevState: IAllPlayers) => {
-                return {...prevState, [position.playerId]: position};
-            })
-
-            if (!playerExists(position.playerId)) {
-                const randomPosition = {
-                    row: getRandomRow(),
-                    col: getRandomColumn(),
-                };
-
-                setPositions((prevState: IAllPositions) => {
-                    return {...prevState, [position.playerId]: [randomPosition]};
-                });
-            }
-        };
-
+        console.log('RECEIVING SUBSCRIPTION')
         apiClientAppSync.hydrated().then((client) => {
             const observable = client.subscribe({
                 query: updatePosition,
