de3e97955671444abd66b42695a2f953d47fa809
Tina <tina.zhou.hui@gmail.com>
2022-04-20 18:50:01 +0100

    feat: can only move three directions

--- src/components/Game/components/NewPlayerLogic.tsx
+++ src/components/Game/components/NewPlayerLogic.tsx
@@ -15,13 +15,25 @@ function NewPlayerLogic({setPlayers, setPositions}: IProps) {
     const screenId = 'asdfsdfasdfsd';
 
     const realtimeResults = (data: IRealTimeData) => {
-
+        // console.log('SETTING PLAYERS AND STATE')
         const position = data.data.onPositionUpdated;
         // console.log('realtime data: ', position);
 
         let allPlayers;
         setPlayers((prevState: IAllPlayers) => {
             allPlayers = prevState;
+
+            const previousDirection = prevState[position.playerId]?.direction;
+            let updatedDirection = position.direction // new direction by default
+
+            // snake can only go to three directions
+            if (previousDirection === 'UP' && updatedDirection === 'DOWN') updatedDirection = 'UP';
+            if (previousDirection === 'RIGHT' && updatedDirection === 'LEFT') updatedDirection = 'RIGHT';
+            if (previousDirection === 'DOWN' && updatedDirection === 'UP') updatedDirection = 'DONW';
+            if (previousDirection === 'LEFT' && updatedDirection === 'RIGHT') updatedDirection = 'LEFT';
+
+            position.direction = updatedDirection
+
             return {...prevState, [position.playerId]: position};
         })
 
@@ -40,7 +52,7 @@ function NewPlayerLogic({setPlayers, setPositions}: IProps) {
 
 
     useEffect(() => {
-        console.log('RECEIVING SUBSCRIPTION')
+        // console.log('RECEIVING SUBSCRIPTION')
         apiClientAppSync.hydrated().then((client) => {
             const observable = client.subscribe({
                 query: updatePosition,
