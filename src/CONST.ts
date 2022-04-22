e76b97ec08aff0b0c06d7d42728648bca9beaaf8
Tina <tina.zhou.hui@gmail.com>
2022-04-23 00:06:37 +0100

    fix: env variables

--- src/CONST.ts
+++ src/CONST.ts
@@ -6,7 +6,7 @@ export const ACTIVE = true;
 export const INACTIVE = false;
 
 
-const rootElement = document.body;
+const rootElement = document.getElementById('root');
 
 export const VERSION: string = rootElement?.getAttribute('data-version') || '';
 export const API_URL: string = rootElement?.getAttribute('data-api-url') || '';
--- src/services/apiClientAppSync.ts
+++ src/services/apiClientAppSync.ts
@@ -3,25 +3,17 @@ import AWSAppSyncClient from 'aws-appsync';
 import {AUTH_TYPE} from 'aws-appsync/lib/client';
 import {API_URL, AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY} from '../CONST';
 
-
-const {
-    REACT_APP_REGION,
-    REACT_APP_ACCESS_KEY_ID,
-    REACT_APP_SECRET_ACCESS_KEY,
-    REACT_APP_URL
-} = process.env;
-
 AWS.config.update({
-    region: REACT_APP_REGION || AWS_REGION,
+    region: AWS_REGION,
     credentials: new AWS.Credentials({
-        accessKeyId: REACT_APP_ACCESS_KEY_ID || AWS_ACCESS_KEY_ID,
-        secretAccessKey: REACT_APP_SECRET_ACCESS_KEY || AWS_SECRET_ACCESS_KEY,
+        accessKeyId: AWS_ACCESS_KEY_ID,
+        secretAccessKey: AWS_SECRET_ACCESS_KEY,
     }),
 });
 
 export default new AWSAppSyncClient({
-    url: REACT_APP_URL || API_URL,
-    region: REACT_APP_REGION || AWS_REGION,
+    url: API_URL,
+    region: AWS_REGION,
     auth: {
         type: AUTH_TYPE.AWS_IAM,
         credentials: AWS.config.credentials!,
