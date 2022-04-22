449ec93c940c66ea5051cc803c3db038c577e246
Tina <tina.zhou.hui@gmail.com>
2022-04-22 23:09:39 +0100

    fix: linking data variables

--- src/CONST.ts
+++ src/CONST.ts
@@ -4,3 +4,12 @@ export const TICK = 400;
 export const MIN_LENGTH = 3;
 export const ACTIVE = true;
 export const INACTIVE = false;
+
+
+const rootElement = document.body;
+
+export const VERSION: string = rootElement?.getAttribute('data-version') || '';
+export const API_URL: string = rootElement?.getAttribute('data-api-url') || '';
+export const AWS_REGION: string = rootElement?.getAttribute('data-aws-region') || 'eu-west-1';
+export const AWS_ACCESS_KEY_ID: string = rootElement?.getAttribute('data-access-key-id') || '';
+export const AWS_SECRET_ACCESS_KEY: string = rootElement?.getAttribute('data-secret-access-key') || '';
--- src/services/apiClientAppSync.ts
+++ src/services/apiClientAppSync.ts
@@ -1,6 +1,7 @@
 import * as AWS from 'aws-sdk';
 import AWSAppSyncClient from 'aws-appsync';
 import {AUTH_TYPE} from 'aws-appsync/lib/client';
+import {API_URL, AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY} from '../CONST';
 
 
 const {
@@ -11,16 +12,16 @@ const {
 } = process.env;
 
 AWS.config.update({
-    region: REACT_APP_REGION!,
+    region: REACT_APP_REGION || AWS_REGION,
     credentials: new AWS.Credentials({
-        accessKeyId: REACT_APP_ACCESS_KEY_ID!,
-        secretAccessKey: REACT_APP_SECRET_ACCESS_KEY!,
+        accessKeyId: REACT_APP_ACCESS_KEY_ID || AWS_ACCESS_KEY_ID,
+        secretAccessKey: REACT_APP_SECRET_ACCESS_KEY || AWS_SECRET_ACCESS_KEY,
     }),
 });
 
 export default new AWSAppSyncClient({
-    url: REACT_APP_URL!,
-    region: REACT_APP_REGION!,
+    url: REACT_APP_URL || API_URL,
+    region: REACT_APP_REGION || AWS_REGION,
     auth: {
         type: AUTH_TYPE.AWS_IAM,
         credentials: AWS.config.credentials!,
