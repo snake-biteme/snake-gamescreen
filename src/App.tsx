3dfff1be2c51d52e9cd06cb8901a01dfb0fdb222
Tina <tina.zhou.hui@gmail.com>
2022-04-22 12:08:37 +0100

    feat: shadows and paddings

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import Game from './components/Game/Game';
 import {hexToRgb, randomIntFromInterval} from './components/utils';
 import Navbar from './components/NavBar/Navbar';
 
-function App() {
-    const [colors, setColors] = useState<string[]>([]);
+function getFormattedColors(colors: string[]) {
     const defaultGradient = `linear-gradient(40deg, rgba(${hexToRgb('#1ad02c')},0.8) , rgba(${hexToRgb('#cecece')}, 0) 90%)`;
     const formattedColor: string[] = [defaultGradient];
 
-
-    for (const color of colors)  {
+    for (const color of colors) {
         formattedColor.push(`linear-gradient(${randomIntFromInterval(0, 360)}deg, rgba(${hexToRgb(color)},0.8), rgba(${hexToRgb(color)},0) 90%)`);
     }
-    const colorString = formattedColor.join(', ');
+
+    return formattedColor.join(', ');
+}
+
+function App() {
+    const [colors, setColors] = useState<string[]>([]);
+
+    const colorString = getFormattedColors(colors);
 
     // set background color to all player's color
     const customBackground = {
