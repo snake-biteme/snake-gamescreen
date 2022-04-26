ea4de0422ed927b5f5706989917123abf7c1968f
Tina <tina.zhou.hui@gmail.com>
2022-04-26 12:52:36 +0100

    feat: cleanup dependepncies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
 import Game from './components/Game/Game';
-
 import Navbar from './components/NavBar/Navbar';
 
 
 function App() {
-
-
-    // set background color to all player's color
+    // to keep the screen from sleep: https://developer.mozilla.org/en-US/docs/Web/API/Screen_Wake_Lock_API
+    // only supported on Chrome
+    (async function keepWake() {
+        if ('wakeLock' in navigator) {
+            // create an async function to request a wake lock
+            try {
+                await navigator.wakeLock.request('screen');
+            } catch (err) {
+                // The Wake Lock request has failed - usually system related, such as battery.
+                console.error('wakeLock Error', err);
+            }
+        }
+    })();
 
     return (
         <>
