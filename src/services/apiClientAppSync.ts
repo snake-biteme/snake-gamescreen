9cd00ec665e152f24f4965c635358b40ae3ebcd9
tinazhouhui <tina.zhou.hui@gmail.com>
2023-04-10 12:53:32 +0200

    refactor: simplify credentials

--- src/services/apiClientAppSync.ts
+++ src/services/apiClientAppSync.ts
@@ -1,21 +1,14 @@
-import * as AWS from 'aws-sdk';
 import AWSAppSyncClient from 'aws-appsync';
 import {AUTH_TYPE} from 'aws-appsync/lib/client';
 import {API_URL, AWS_ACCESS_KEY_ID, AWS_REGION, AWS_SECRET_ACCESS_KEY} from '../consts';
-
-AWS.config.update({
-    region: AWS_REGION,
-    credentials: new AWS.Credentials({
-        accessKeyId: AWS_ACCESS_KEY_ID,
-        secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    }),
-});
-
 export default new AWSAppSyncClient({
     url: API_URL,
     region: AWS_REGION,
     auth: {
         type: AUTH_TYPE.AWS_IAM,
-        credentials: AWS.config.credentials!,
+        credentials: {
+            accessKeyId: AWS_ACCESS_KEY_ID,
+            secretAccessKey: AWS_SECRET_ACCESS_KEY,
+        }
     },
 });
